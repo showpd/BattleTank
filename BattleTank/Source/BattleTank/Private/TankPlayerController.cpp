f1e8f06969471ac7f7590fb35344259a6ac543c0
Show Philis Dev <showphilisdev@gmail.com>
2018-08-17 17:12:09 -0300

    BT07

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -5,7 +5,20 @@
 #include "BattleTank.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
 
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing: %s"), *(ControlledTank->GetName()));
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
